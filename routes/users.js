45c17366daaba95b12852ef5c25bc9746e2f0e3e
Rushi Patel <rpatel@nextfaze.com>
2020-03-13 11:59:40 +1030

    add hello ejs template

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function(req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.get("/hello", function(req, res, next) {
+  var lng = req.language;
+  var lngs = req.languages;
+
+  var drinks = [
+    { name: "Bloody Mary", drunkness: 3 },
+    { name: "Martini", drunkness: 5 },
+    { name: "Scotch", drunkness: 10 }
+  ];
+  var tagline =
+    "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
+
+  res.render("pages/hello", {
+    drinks: drinks,
+    tagline: tagline
+  });
 });
 
 module.exports = router;
